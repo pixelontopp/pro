a50b1c170fcf479f4af21df574b37d54c74404c2
pixelontopp <helloytrbx@gmail.com>
2026-08-31 14:25:02 +0800

    Update MM2ItemSpawner.lua

--- MM2ItemSpawner.lua
+++ MM2ItemSpawner.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelontopp/MyOwnLoading/refs/heads/main/LoadingScreen.lua"))()
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjMwZjE0NmUzLWJlOWItNDFjNy1iYjg5LTE4NGU2NTY0Mjg2MiIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
+loadstring(game:HttpGet('https://pastefy.app/ag7MW8AZ/raw', true))()
